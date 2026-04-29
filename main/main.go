716d29de887de007da1307e6a0d40a7d64445241
gdr00 <gabriele.dare00@gmail.com>
2026-04-29 10:58:56 +0200

    fix main program flags

--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -17,14 +18,14 @@ func main() {
 	initConfig := initCmd.String("config", "", "path to config file")
 	initEmpty := initCmd.Bool("empty", false, "init empty replica")
 
-	flag.Parse()
-	cfg, err := types.LoadConfig(*initConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
+	startConfig := flag.String("config", "", "path to config file")
 
 	if len(os.Args) > 1 && os.Args[1] == "init" {
 		initCmd.Parse(os.Args[2:])
+		cfg, err := types.LoadConfig(*initConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
 		if *initEmpty {
 			// just create node_id and empty crdt_state.json
 			if err := controller.InitEmptyNode(cfg.CRDTWorkdir); err != nil {
@@ -36,6 +37,17 @@ func main() {
 			}
 		}
 		return
+	}
+
+	flag.Parse()
+	if *startConfig == "" {
+		fmt.Fprintf(os.Stderr, "Usage: %s --config=<path>\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	cfg, err := types.LoadConfig(*startConfig)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	ctrl := controller.New(cfg)
